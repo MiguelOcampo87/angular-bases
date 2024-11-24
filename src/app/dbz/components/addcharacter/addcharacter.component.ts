d75e0ae2d3d75f65657fdd7f4ee91299857cdab5
Miguel Ocampo <miguel.ocampog@gmail.com>
2024-11-24 00:48:30 -0600

    modulo DBZ inicio

---
+++ src/app/dbz/components/addcharacter/addcharacter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-addcharacter',
+  templateUrl: './addcharacter.component.html',
+  styleUrl: './addcharacter.component.css'
+})
+export class AddcharacterComponent {
+
+}
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddcharacterComponent } from './components/addcharacter/addcharacter.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    AddcharacterComponent
+  ],
+  exports:[
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
