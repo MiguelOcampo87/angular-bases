0d965747f920e3e1374a4a5a452bc83088f4c8ee
Miguel Ocampo <miguel.ocampog@gmail.com>
2024-11-26 22:20:31 -0600

    add character component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './pages/main-page.component';
-import { ListComponent } from './components/list/list.component';
+import { FormsModule } from '@angular/forms';
+
 import { AddcharacterComponent } from './components/addcharacter/addcharacter.component';
-
-
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { AddcharacterComponent } from './components/addcharacter/addcharacter.co
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
