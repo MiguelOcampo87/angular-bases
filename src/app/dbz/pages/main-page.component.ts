0480a4d150913cdcefed10181238a8b27b2feb62
Miguel Ocampo <miguel.ocampog@gmail.com>
2024-12-01 08:31:10 -0600

    creacion servicio

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,27 +8,19 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent {
-  public characters:Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 9200
-    },
-  ];
 
-  onNewCharacter(character:Character):void {
-    this.characters.push(character);
+  constructor( private dbzService: DbzService ) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
   }
 
-  onDeleteCharacter(index:number){
-    this.characters.splice(index);
+  onDeleteCharacterById( id: string ):void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter ( character: Character):void {
+    this.dbzService.addCharacter( character );
   }
 
 }
